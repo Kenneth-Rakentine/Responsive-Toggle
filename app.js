f543fbb3c169a14d93b93e20b7242d497a14adbf
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-06-13 13:33:18 -0400

    second commit

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
 
-const boxToCircle = () =>{
-    // 1.) target box
-    let square = document.querySelector('.box')
-    square.classList.toggle('circle')
-}
-
-const boxToBio = ()=>{
-    let square = document.querySelector('.box')
-    square.classList.toggle('bio')
-}
-
-const showBio = ()=>{
-    boxToBio()
-    let newBox = document.querySelector('.bio')
-    newBox.innerHTML = "This represents "
-}
-
-const hideImage = ()=>{
-    let target = document.querySelector('.jordan')
-    target.classList.toggle('disappear')
-    let btn = document.querySelector('.hidden')
-    btn.innerHTML = "Show Me"
-}
-
-
 // __________________________________________________
 
 
